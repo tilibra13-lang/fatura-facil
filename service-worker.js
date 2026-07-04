6e77efa59171ce1b01f794211d7e97e5cc3240c7
tilibra13-lang <tilibra13@gmail.com>
2026-07-04 11:04:42 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME="fatura-facil-v15";
+const CACHE_NAME="fatura-facil-v16";
 const FILES=["./","./index.html","./manifest.json"];
 self.addEventListener("install",event=>{event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(FILES)))});
 self.addEventListener("activate",event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(key=>key!==CACHE_NAME?caches.delete(key):null))))});
